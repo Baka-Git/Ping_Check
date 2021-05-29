718e73cb69860da8b7d7a50dd0b92666e9ae55bc
Baka-Git <80925229+Baka-Git@users.noreply.github.com>
2021-05-29 12:14:44 +0200

    Fix unreachble bug

--- modules/ping_module.py
+++ modules/ping_module.py
@@ -13,12 +13,23 @@ class Host:
     def ping(self):
         argument = "-c"
         if platform.system().lower() == "windows":
-            argument = "-n"
-
-        value = subprocess.call(["ping", argument, "1", self.address])
+            process = subprocess.Popen(["ping", "-n", "1", self.address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            communication = process.communicate()[0]
+            value = 0
+            if 'unreachable' in str(communication) or 'timed' in str(communication):
+                value=1
+        else:
+            value = subprocess.call(["ping", "-c", "1", self.address])
         self.total_ping += 1
         if value != 0:
             self.fail_ping += 1
             return False
         self.last_response = time.time()
         return True
+
+#def ping(host):
+  #  process = subprocess.Popen(["ping", "-n", "1",host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   # streamdata = process.communicate()[0]
+    #if 'unreachable' in str(streamdata):
+    #    return 1
+   # return process.returncode
